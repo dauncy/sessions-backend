2805bbc2044e8fb55a6688694d6b4a3fbb870485
Your_Name <joseph161216@gmail.com>
2020-01-21 16:35:37 -0800

    comment out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-    User.create(username: "krazyfrog",
-                 name: "Danny", 
-                 password: "123", 
-                 password_confirmation: '123', 
-                 bio: "All Day EveryDay", 
-                 location: "San Francisco")
+    # User.create(username: "krazyfrog",
+    #              name: "Danny", 
+    #              password: "123", 
+    #              password_confirmation: '123', 
+    #              bio: "All Day EveryDay", 
+    #              location: "San Francisco")
     
     
     
-    User.create(username: "Jojo",
-                 name: "Joseph", 
-                 password: "abc", 
-                 password_confirmation: 'abc', 
-                 bio: "All Night Every Night", 
-                 location: "San Francisco")
+    # User.create(username: "Jojo",
+    #              name: "Joseph", 
+    #              password: "abc", 
+    #              password_confirmation: 'abc', 
+    #              bio: "All Night Every Night", 
+    #              location: "San Francisco")
 
 
-    5.times do 
-        user = User.all.sample
-        Message.create(user_id: user.id, text: Faker::Quote.yoda)
-    end 
+    # 5.times do 
+    #     user = User.all.sample
+    #     Message.create(user_id: user.id, text: Faker::Quote.yoda)
+    # end 
 
     
